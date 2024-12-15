b85bd907a78ee43bf923de1a1a9938edfedf3035
Marcin <marcin.slonka@student.uj.edu.pl>
2024-12-15 17:49:38 +0100

    Zrobiono zadanie 2

--- ZADANIE2/reduce_function.py
+++ ZADANIE2/reduce_function.py
@@ -1,10 +1,20 @@
 from fractions import Fraction
 from functools import reduce
+from math import gcd
 
+def fracMultiplication(a: Fraction, b: Fraction):
+    num = a.numerator * b.numerator
+    den = a.denominator * b.denominator
+    greatestCommDiv = gcd(num, den)
+    if greatestCommDiv != 1:
+        num //= greatestCommDiv
+        den //= greatestCommDiv
+        
+    return Fraction(num, den)
 
 def product(fracs):
-# twoj kod tutaj
-    pass 
+    t = reduce(fracMultiplication, fracs)
+    return t.numerator, t.denominator
 
 
 if __name__ == "__main__":
